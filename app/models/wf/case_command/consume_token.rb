b209f83bbd3a9ec8dbbf956e090f9a1bb7146c56
Hooopo Wang <hoooopo@gmail.com>
2020-01-23 20:28:21 +0800

    add consume token

---
+++ app/models/wf/case_command/consume_token.rb
@@ -0,0 +1,19 @@
+module Wf::CaseCommand
+  class ConsumeToken
+    prepend SimpleCommand
+    attr_reader :workitem, :wf_case, :locked_item
+    def initialize(wf_case, place, locked_item)
+      @workitem = workitem
+      @wf_case  = wf_case
+      @locked_item = locked_item
+    end
+
+    def call
+      if locked_item
+        wf_case.tokens.where(place: place, state: :locked, locked_workitem_id: locked_item.id).update!(consumed_at: Time.now, state: :consumed)
+      else
+        wf_case.tokens.where(id: wf_case.tokens.where(place: place, state: :free).limit(1)).update!(consumed_at: Time.now, state: :consumed)
+      end
+    end
+  end
+end

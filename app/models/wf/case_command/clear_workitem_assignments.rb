82e02675d8751a5f1d5f1524d07d40d37b5f68b1
Hooopo Wang <hoooopo@gmail.com>
2020-01-27 23:22:05 +0800

    clear workitem assignments

---
+++ app/models/wf/case_command/clear_workitem_assignments.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Wf::CaseCommand
+  class ClearWorkitemAssignments
+    prepend SimpleCommand
+    attr_reader :workitem, :permanent
+    def initialize(workitem, permanent = true)
+      @workitem = workitem
+      @permanent = permanent
+    end
+
+    def call
+      ClearManualAssignments.call(workitem.case, workitem.transition) if permanent
+      workitem.workitem_assignments.delete_all
+      workitem.transition.unassignment_callback.constantize.new(workitem.id).perform
+    end
+  end
+end

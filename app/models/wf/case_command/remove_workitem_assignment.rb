3d1249823ab2e01acd3305f65fe885b063a07f77
Hooopo Wang <hoooopo@gmail.com>
2020-01-28 02:08:18 +0800

    remove workitem assignment

---
+++ app/models/wf/case_command/remove_workitem_assignment.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Wf::CaseCommand
+  class RemoveWorkitemAssignment
+    prepend SimpleCommand
+    attr_reader :workitem, :party, :permanent
+    def initialize(workitem, party, permanent = true)
+      @workitem = workitem
+      @party    = party
+      @permanent = permanent
+    end
+
+    def call
+      return if party.nil?
+
+      RemoveManualAssignment.call(workitem.case, workitem.transition, party) if permanent
+      workitem.workitem_assignments.where(party: party).destroy!
+
+      workitem.transition.unassignment_callback.constantize.new(workitem.id).perform_now if workitem.workitem_assignments.count == 0
+    end
+  end
+end
